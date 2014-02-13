ec4bce9fc415027d49a598b6b34a16c1d7ea7d2e
Nick Newell <blotto@gmail.com>
2014-02-12 16:22:14 -0800

    extend relation methods

--- lib/thermometer/active_record.rb
+++ lib/thermometer/active_record.rb
@@ -8,9 +8,29 @@ module Thermometer
         else
           load
         end
-        return size
+        evaluate_level(time_diff_for(self.first.send(Thermometer.configuration.date)))
       end
 
+
+      private
+
+      def evaluate_level(days, ranges=Thermometer.configuration.default_time_range)
+
+        level = :none
+        ranges.each do |k,v|
+          if v.include?(days)
+            level = k
+            break
+          end
+        end
+        return level
+      end
+
+      def time_diff_for(date, increment=:days)
+        now = Time.at(DateTime.now)
+        diff = (now - date).to_i
+        (diff.to_f/1.send(increment)).send(increment)
+      end
 
     end
 =begin
