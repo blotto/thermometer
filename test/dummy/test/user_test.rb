9878dac2c332bae1607763bae518ad5073652416
Nick Newell <blotto@gmail.com>
2014-02-20 11:18:02 -0800

    try namespacing fixture class

--- test/dummy/test/user_test.rb
+++ test/dummy/test/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  set_fixture_class 'dummy/models/user' =>  User
+
+
    test "instance should respond to has_temperature" do
      assert users(:user_1).respond_to?(:has_temperature)
    end
