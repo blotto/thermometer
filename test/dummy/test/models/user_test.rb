7420e576496025305459b6be1c3797e832c0b5cc
Nick Newell <blotto@gmail.com>
2014-02-20 14:26:35 -0800

    clean up

--- test/dummy/test/models/user_test.rb
+++ test/dummy/test/models/user_test.rb
@@ -2,10 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  #set_fixture_class :users => User
-
    test "instance should respond to has_temperature" do
-     p users(:user_1)
      assert users(:user_1).respond_to?(:has_temperature)
    end
 end
