a1d2d73f016669d9d740949a196bab8029f8cc72
Nick Newell <blotto@gmail.com>
2014-02-20 14:54:53 -0800

    increase test cov

--- test/dummy/test/models/user_test.rb
+++ test/dummy/test/models/user_test.rb
@@ -5,4 +5,34 @@ class UserTest < ActiveSupport::TestCase
    test "instance should respond to has_temperature" do
      assert users(:user_1).respond_to?(:has_temperature)
    end
+
+   test "instance should respond to has_temperature?" do
+     assert users(:user_1).respond_to?(:has_temperature?)
+   end
+
+   test "instance should respond to is_colder_than?" do
+     assert users(:user_1).respond_to?(:is_colder_than?)
+   end
+
+   test "instance should respond to is_warmer_than?" do
+     assert users(:user_1).respond_to?(:is_warmer_than?)
+   end
+
+   test "instance association messages should respond to has_temperature" do
+     assert users(:user_1).messages.respond_to?(:has_temperature)
+   end
+
+   test "instance association messages should respond to has_temperature?" do
+     assert users(:user_1).messages.respond_to?(:has_temperature?)
+   end
+
+   test "instance association messages should respond to is_colder_than?" do
+     assert users(:user_1).messages.respond_to?(:is_colder_than?)
+   end
+
+   test "instance association messages should respond to is_warmer_than?" do
+     assert users(:user_1).messages.respond_to?(:is_warmer_than?)
+   end
+
+
 end
