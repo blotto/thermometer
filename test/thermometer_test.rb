ef87649be2788e5bdfdb3835cb006f1a32e0465f
Nick Newell <blotto@gmail.com>
2014-02-21 16:45:07 -0800

    increase coverage

--- test/thermometer_test.rb
+++ test/thermometer_test.rb
@@ -5,4 +5,28 @@ class ThermometerTest < ActiveSupport::TestCase
     assert_kind_of Module, Thermometer
   end
 
+  test "Thermometer yields a configuration" do
+    Thermometer.configure do |c|
+      assert_instance_of Thermometer::Configuration ,  c
+    end
+  end
+
+  test "Configuration as a config Hash" do
+    assert_kind_of Hash, Thermometer.configuration.config,
+  end
+
+  test "Configuration has a config Hash" do
+    assert_kind_of Hash, Thermometer.configuration.config,
+  end
+
+  test "Configuration has a time_ranges Hash" do
+    assert_kind_of ActiveSupport::HashWithIndifferentAccess, Thermometer.configuration.time_ranges,
+  end
+
+  test "Configuration has a scope_options Hash" do
+    assert_kind_of Hash, Thermometer.configuration.scope_options,
+  end
+
+
+
 end
